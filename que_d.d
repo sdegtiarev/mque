4d0cf1d0177900a0e219330f36c1dc143e9c4ff8
Sergei Degtiarev <sdegtiarev@yahoo.com>
2016-01-30 23:08:46 -0500

    d cosmetic touch

--- que_d.d
+++ que_d.d
@@ -8,9 +8,9 @@ void main()
 	foreach(i; 0..N) {
 		auto t=MonoTime.currTime;
 		tid.send(thisTid, t.ticks);
+		// wait receiver for 
 		receiveOnly!int();
 	}
-	Thread.sleep(seconds(1));
 }
 
 
@@ -19,6 +19,7 @@ void f(int n)
 	foreach(i; 0..n) {
 		auto m=receiveOnly!(Tid,long)();
 		writeln(MonoTime.currTime.ticks-m[1]);
+		// ask for next message
 		m[0].send(0);
 	}
 }
